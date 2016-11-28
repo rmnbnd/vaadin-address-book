e1513968d035a891014f4996451dedee9e899479
Roman Bondarchuk <rmnbnd@gmail.com>
2016-11-28 23:48:03 +0200

    Create contact form with fields

--- src/main/java/vaadin/addressbook/ui/view/ContactFormView.java
+++ src/main/java/vaadin/addressbook/ui/view/ContactFormView.java
@@ -1,26 +1,42 @@
 package vaadin.addressbook.ui.view;
 
+import com.vaadin.event.ShortcutAction;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.spring.annotation.SpringView;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
-
-import javax.annotation.PostConstruct;
+import com.vaadin.ui.themes.ValoTheme;
 
 @SpringView(name = ContactFormView.VIEW_NAME)
 public class ContactFormView extends VerticalLayout implements View {
 
     public static final String VIEW_NAME = "contact";
 
-    @PostConstruct
-    void init() {
-        setMargin(true);
+    public ContactFormView() {
+        TextField name = new TextField("Name");
+        TextField email = new TextField("Email");
+        TextField phone = new TextField("Phone");
+        Button cancel = new Button("Cancel");
+        Button save = new Button("Save", FontAwesome.SAVE);
+        CssLayout actions = new CssLayout(save, cancel);
+
+        // Aff components
+        addComponents(name, email, phone, actions);
+
+        // Configure and style components
         setSpacing(true);
-        addComponent(new Label("This is a contact form"));
+        setMargin(true);
+        actions.setStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+        save.setStyleName(ValoTheme.BUTTON_PRIMARY);
+        save.setClickShortcut(ShortcutAction.KeyCode.ENTER);
     }
 
     @Override
     public void enter(ViewChangeListener.ViewChangeEvent event) {
     }
+
 }
